712e799355237dea058712cf05df111003b532d7
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2020-02-01 17:25:32 -0500

    PlayerScore

---
+++ Assets/PlayerBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    public GameObject owner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var GO = other.gameObject;
+        if(GO == owner)
+        {
+            var controller = other.GetComponent<ItemController>();
+            if (controller.currentRepairItem != null)
+            {
+                GameManager.Instance.ScorePoint(GO.GetComponent<CharacterMovementController>().playerID);
+                Destroy(controller.currentRepairItem.gameObject);
+                controller.currentRepairItem = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int player1Score = 0;
+    public int player2Score = 0;
+    public int scoreToWin = 5;
+    public List<RepairSpawnPoint> repairSpawnPoints;
+
+    static GameManager instance;
+
+    public static GameManager Instance { get { return instance; } }
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        SpawnRepairItem();
+    }
+
+    public void SpawnRepairItem()
+    {
+        System.Random random = new System.Random();
+        repairSpawnPoints[random.Next(repairSpawnPoints.Count)].SpawnItem();
+    }
+
+    public void ScorePoint(int playerId)
+    {
+        if(playerId == 0)
+        {
+            player1Score++;
+            CheckForWin(playerId, player1Score);
+        }
+        else if(playerId == 1)
+        {
+            player2Score++;
+            CheckForWin(playerId, player2Score);
+        }
+        SpawnRepairItem();
+    }
+
+    public void CheckForWin(int playerId, int playerScore)
+    {
+        if(playerScore >= scoreToWin)
+        {
+            Debug.Log($"Player {playerId + 1} wins!");
+        }
+    }
+}
