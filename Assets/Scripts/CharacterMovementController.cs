08e67628675b76f886fd6d284e3b1f41f9302014
Alexis Parent <alexis.parent.1@ens.etsmtl.ca>
2020-01-31 22:00:47 -0500

    basic movements

---
+++ Assets/Scripts/CharacterMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float movementSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        controller.Move(new Vector3(x, 0, y) * Time.deltaTime * movementSpeed);
+    }
+}
