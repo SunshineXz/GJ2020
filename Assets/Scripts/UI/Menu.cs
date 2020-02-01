0a4398054326bc4be6e61b38f4161b5286a062d0
Alexis Parent <alexis.parent.1@ens.etsmtl.ca>
2020-02-01 18:03:58 -0500

    Fix main menu input settings

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -8,7 +8,7 @@ public class Menu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitGame()
