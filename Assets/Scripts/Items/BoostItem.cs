0067b1cb21e38ebab3ca0570ee90e5f4d996beca
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2020-02-01 01:22:38 -0500

    Items start

---
+++ Assets/Scripts/Items/BoostItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostItem : EffectItem
+{
+    public override void Use(CharacterMovementController characterMovementController)
+    {
+        characterMovementController.SetMovementSpeedToSlow();
+    }
+}
---
+++ Assets/Scripts/Items/RepairItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class RepairItem : Item
+{
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var itemController = other.GetComponent<ItemController>();
+            if (itemController.currentRepairItem == null)
+            {
+                itemController.PickUpRepairItem(this);
+                Destroy();
+            }
+        }
+    }
+}
