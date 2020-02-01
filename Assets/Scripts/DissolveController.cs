a4720b921e637667cc303a2b32ca5e8ce3531b2f
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-02-01 04:59:52 -0500

    added shader for dissolving and some param switching enter the commit message for your changes. Lines starting Shader.shadergraphasqwqwqwqwqwqw12121`121211edwmbmv,,.,.,.,.
    S
    aswewewewewewewewewewewewe

---
+++ Assets/Scripts/DissolveController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    public Material dissolveMat;
+    public float value;
+    public float maxValue;
+
+    void Start()
+    {
+        dissolveMat.SetFloat("", value / maxValue);
+    }       
+}
