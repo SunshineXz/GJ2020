10788fede76efcfd7b9e2f973cf0b30500386804
Anthony Labelle-Voyez <antho.voyez@hotmail.com>
2020-02-02 14:46:43 -0500

    Victory Screen

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -8,12 +8,13 @@ public class Menu : MonoBehaviour
 
     public void PlayGame()
     {
+        FindObjectOfType<AudioManager>().Play("Button");
         SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
     {
+        FindObjectOfType<AudioManager>().Play("Button");
         Application.Quit();
     }
-
 }
---
+++ Assets/VictoryController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryController : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+
+    public Material p1Mat;
+    public Material p2Mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("Theme");
+        var gameManager = GameManager.Instance;
+
+        if(gameManager.player1Score > gameManager.player2Score)
+        {
+            Player1.GetComponent<Renderer>().material = p1Mat;
+            Player2.GetComponent<Renderer>().material = p2Mat;
+        }
+        else
+        {
+            Player1.GetComponent<Renderer>().material = p2Mat;
+            Player2.GetComponent<Renderer>().material = p1Mat;
+        }
+    }
+}
