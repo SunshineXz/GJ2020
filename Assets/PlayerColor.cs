81be8d8ce4c8838cd68a1d233a90aaccbf2a2685
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-02-02 11:11:41 -0500

    Color improvements

--- Assets/PlayerColor.cs
+++ Assets/PlayerColor.cs
@@ -1,14 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerColor : MonoBehaviour
 {
+    private static List<bool> usedList;
+    private static List<ColorTableTuple> colorTable;
+    public List<Color> colorList;
     [SerializeField] private SkinnedMeshRenderer rend;
-    private static float variation = 0f;
+
     void Start()
     {
-        rend.materials[0].SetColor("_BaseColor",Random.ColorHSV(variation, variation+0.5f, 1f, 1f, 0.5f, 1f));
-        variation = 0.5f;
+        if(colorTable == null)
+        {
+            colorTable = new List<ColorTableTuple>();
+            for(int i = 0; i < colorList.Count; i++)
+            {    
+                colorTable.Add(new ColorTableTuple(){Color = colorList[i],Used = false});
+            }
+            
+        }
+        var availColors = colorTable.Where(x => x.Used == false).ToList();
+        var index = Random.Range(0, availColors.Count);
+        var newColor = availColors.ElementAt(index).Color;
+        rend.materials[0].SetColor("_BaseColor",newColor);
+        colorTable[index] = new ColorTableTuple(){Color = newColor,Used = true};
+
+
+    }
+
+    public struct ColorTableTuple
+    {
+        public Color Color;
+        public bool Used;
     }
 }
