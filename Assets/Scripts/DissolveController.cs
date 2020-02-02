2744f8aa834687036e65eccc45c787475739c377
BrokenSm4sh <charles.sterlin@hotmail.com>
2020-02-02 02:45:19 -0500

    Shaders and dissolve shader script

--- Assets/Scripts/DissolveController.cs
+++ Assets/Scripts/DissolveController.cs
@@ -1,15 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// countdown of property dissolve time from up to 1 to -1
+/// </summary>
 public class DissolveController : MonoBehaviour
 {
-    public Material dissolveMat;
-    public float value;
+    public Renderer rend;
+    
+    private Material dissolveMat;
+    
+    [Range(0,1)]
+    public float dissolveRate;
+    
+    private float currentDissolve;
+    
+    [Range(-1,1)]
+    public float minValue;
+    
+    [Range(-1,1)]
     public float maxValue;
 
     void Start()
     {
-        dissolveMat.SetFloat("", value / maxValue);
-    }       
+        dissolveMat = rend.material;
+        currentDissolve = maxValue;
+        // minValue /= 100;
+        // maxValue /= 100;
+        // dissolveRate /= 100;
+        dissolveMat.SetFloat("Vector1_3F3EEC92",1f);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed space");
+            StepDissolve();
+        }
+    }
+
+    void StepDissolve()
+    {
+        if (currentDissolve >= minValue)
+        {
+            Debug.Log("resolving space"+ dissolveMat.GetFloat("Vector1_3F3EEC92"));
+
+            currentDissolve -= dissolveRate;
+            rend.material.SetFloat("Vector1_3F3EEC92",currentDissolve);
+        }
+    }
 }
